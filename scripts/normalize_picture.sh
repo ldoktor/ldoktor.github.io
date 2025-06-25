144203cd9a2abeb7dcae190b8526900425e70a60
Lukáš Doktor <ldoktor@post.cz>
2025-06-25 17:11:51 +0200

    scripts.normalize: Print the listing
    Signed-off-by: Lukáš Doktor <ldoktor@post.cz>

--- scripts/normalize_picture.sh
+++ scripts/normalize_picture.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
+OUT=""
+LAST_GRP=""
 for SRC in $*; do
-    EXT="$(echo "$SRC" | rev | cut -d'.' -f1 | rev)"
-    SMALL="${SRC:0:-$(expr ${#EXT} + 1)}-small.$EXT"
-    if [ -e "$SMALL" ]; then
-        echo "Existing $SMALL"
-    else
-        echo "Creating $SMALL"
-        jpegoptim -m 80 "$SRC"
-        convert "$SRC" -resize 150x150 -quality 60 "$SMALL"
-    fi
+	if [[ $SRC =~ ^(.*)-([0-9]+)-([0-9]+)-(.*)\.jpg$ ]]; then
+		GROUP="${BASH_REMATCH[1]}"
+		SEMESTER="${BASH_REMATCH[2]}"
+		CLASS="${BASH_REMATCH[3]}"
+		TOPIC="${BASH_REMATCH[4]}"
+		if [[ $SRC =~ ^(.*)-small.jpg$ ]]; then
+			echo "Ignoring $SRC"
+			continue
+		fi
+
+		if [[ "$LAST_GRP" != "$GROUP" ]]; then
+			OUT+="\n"
+			LAST_GRP="$GROUP"
+		fi
+
+		OUT+="<a href=\"$SRC\">\n"
+		OUT+="    <img align=\"right\" src=\"${GROUP}-${SEMESTER}-${CLASS}-${TOPIC}-small.jpg\" style=\"height:85px\">\n"
+		OUT+="</a>\n"
+
+		EXT="$(echo "$SRC" | rev | cut -d'.' -f1 | rev)"
+		SMALL="${SRC%.jpg}-small.$EXT"
+		if [ -e "$SMALL" ]; then
+			echo "Existing $SMALL"
+		else
+			echo "Creating $SMALL"
+			jpegoptim -m 80 "$SRC" 2>&1
+			convert "$SRC" -resize 150x150 -quality 60 "$SMALL" 2>&1
+		fi
+	else
+		echo "Incorrect file name $SRC"
+	fi
 done
+
+echo
+echo -e "$OUT"
